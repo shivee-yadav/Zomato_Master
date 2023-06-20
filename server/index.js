a0f277fb292beee739eeedc5505b057f586044a0
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-20 14:16:59 +0530

    Server SetUp

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ const express = require("express");
 //require is old feature,import is es6 feature
 //babel - enabling es6
 
+const zomato = express();
+
+zomato.get("/", (req,res) => res.json({message: "SetUp Success!!!"}));
+
+zomato.listen(4000, () => console.log("Server is up and running"));
+
