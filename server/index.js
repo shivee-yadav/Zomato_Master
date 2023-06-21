63579947653177d1d511ed4a780425a96097d741
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-21 16:19:04 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 //require is old feature,import is es6 feature
 //babel - enabling es6
 import cors from "cors";
 import helmet from "helmet";
+
+import Auth from "./API/Auth"
+
+import connection from "./database/connection";
 
 const zomato = express();
 
@@ -11,9 +17,15 @@ zomato.use(express.urlencoded({extended: false}));
 zomato.use(helmet());
 zomato.use(cors());
 
+//for application routes
+//loacalhost:4000/auth/signup
+zomato.use("/auth", Auth);
+
 zomato.get("/", (req,res) => res.json({message: "SetUp Success!!!"}));
 
-zomato.listen(4000, () => console.log("Server is up and running"));
+zomato.listen(4000, () => 
+connection().then(() => console.log("Server is up and running"))
+.catch(()=>console.log("DB connection failed")));
 
 //cors->package to maintain so many API calls
 //helmet ->added for extra security layers
