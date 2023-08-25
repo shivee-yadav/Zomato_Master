af41cc8725cef00b1b424fa60b66a0e909b636a6
Shivee Yadav <shiveeyadav089@gmail.com>
2023-08-25 23:38:58 +0530

    new commit

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
 import DeliveryCarousal from "./DeliveryCarousal";
 
 const Delivery = () => {
+    // const [restaurantList, setRestaurantList] = useState([]);
+
+    // const reduxState = useSelector(
+    //     (globalStore) => globalStore.restaurant.restaurants
+    // );
+
+    // useEffect(()=> {
+    //     setRestaurantList(reduxState.restaurants);
+    // }, [reduxState.restaurants]);
+
     return(
         <>
         <DeliveryCarousal />
--- client/src/Layout/Home.layout.js
+++ client/src/Layout/Home.layout.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 import Navbar from "../Components/Navbar";
 import FoodTab from "../Components/FoodTab";
 
+//import { getRestaurant } from "../Redux/Reducer/restaurant/restaurant.action";
+
 const HomeLayout = (props) => {
+
+    // const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     dispatch(getRestaurant());
+    // },[]);
+
     return (
     <>
     <div className="container mx-auto lg:px-20">
